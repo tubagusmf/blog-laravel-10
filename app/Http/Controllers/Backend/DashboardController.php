eaa86227661c62f0f78077a08910ecf553df4765
Tubagus M Fadillah <tubagusfadillah98@gmail.com>
2024-02-06 20:16:49 +0700

    update Dashboard Admin
    menambahkan total articles, total category, latest article, & popular article

--- app/Http/Controllers/Backend/DashboardController.php
+++ app/Http/Controllers/Backend/DashboardController.php
@@ -2,13 +2,20 @@
 
 namespace App\Http\Controllers\Backend;
 
-use App\Http\Controllers\Controller;
+use App\Models\Article;
+use App\Models\Category;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('backend.dashboard.index');
+        return view('backend.dashboard.index', [
+            'total_articles'    => Article::count(),
+            'total_categories'  => Category::count(),
+            'latest_article'    => Article::with('Category')->whereStatus(1)->latest()->take(5)->get(), //mengambil artikel terbaru dari status publish(1) & mengambil 5 artikel
+            'popular_article'   => Article::with('Category')->whereStatus(1)->orderBy('views', 'desc')->take(5)->get(), //mengambil artikel popular dari banyak views & mengambil 5 artikel
+        ]);
     }
 }
--- resources/views/backend/dashboard/index.blade.php
+++ resources/views/backend/dashboard/index.blade.php
@@ -10,19 +10,93 @@
 <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
       <h1 class="h2">Dashboard</h1>
-      <div class="btn-toolbar mb-2 mb-md-0">
-        <div class="btn-group me-2">
-          <button type="button" class="btn btn-sm btn-outline-secondary">Share</button>
-          <button type="button" class="btn btn-sm btn-outline-secondary">Export</button>
+    </div>
+
+    <div class="row">
+      <div class="col-6">
+        <div class="card text-bg-primary mb-3" style="max-width: 100%;">
+          <div class="card-header">Total Article</div>
+          <div class="card-body">
+            <h5 class="card-title">{{ $total_articles }} Articles</h5>
+            <p class="card-text">
+              <a href="{{ url('article') }}" class="text-white">View</a>
+            </p>
+          </div>
         </div>
-        <button type="button" class="btn btn-sm btn-outline-secondary dropdown-toggle">
-          <span data-feather="calendar"></span>
-          This week
-        </button>
+      </div>
+      <div class="col-6">
+        <div class="card text-bg-secondary mb-3" style="max-width: 100%;">
+          <div class="card-header">Total Categories</div>
+          <div class="card-body">
+            <h5 class="card-title">{{ $total_categories }} Categories</h5>
+            <p class="card-text">
+              <a href="{{ url('categories') }}" class="text-white">View</a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+    <div class="row">
+      <div class="col-6">
+        <h4>Latest Article</h4>
+        <table class="table table-bordered table-striped">
+          <thead>
+            <tr>
+              <th>No</th>
+              <th>Title</th>
+              <th>Category</th>
+              <th>Created At</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            @foreach ($latest_article as $item)
+                <tr>
+                  <td>{{ $loop->iteration }}</td>
+                  <td>{{ $item->title }}</td>
+                  <td>{{ $item->category->name }}</td>
+                  <td>{{ $item->created_at }}</td>
+                  <td class="text-center">
+                    <a href="{{ url('article/'.$item->id) }}" class="btn btn-secondary btn-sm">Detail</a>
+                  </td>
+                </tr>
+            @endforeach
+          </tbody>
+        </table>
+      </div>
+
+      <div class="col-6">
+        <h4>Popular Article</h4>
+        <table class="table table-bordered table-striped">
+          <thead>
+            <tr>
+              <th>No</th>
+              <th>Title</th>
+              <th>Category</th>
+              <th>Created At</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            @foreach ($latest_article as $item)
+                <tr>
+                  <td>{{ $loop->iteration }}</td>
+                  <td>{{ $item->title }}</td>
+                  <td>{{ $item->category->name }}</td>
+                  <td>{{ $item->created_at }}</td>
+                  <td class="text-center">
+                    <a href="{{ url('article/'.$item->id) }}" class="btn btn-secondary btn-sm">Detail</a>
+                  </td>
+                </tr>
+            @endforeach
+          </tbody>
+        </table>
       </div>
     </div>
 
-    <canvas class="my-4 w-100" id="myChart" width="900" height="380"></canvas>
 
   </main>
 </div>
